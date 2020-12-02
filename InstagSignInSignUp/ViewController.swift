ee03b54a8c4239aff14e493406afa0865ab80829
Zekhniddin <zexjumayev@gmail.com>
2020-12-02 22:07:20 +0500

    Initial Commit

---
+++ InstagSignInSignUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagSignInSignUp
+//
+//  Created by Зехниддин on 12/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
